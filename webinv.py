49f7b470e6ac0642b6526f3a8259826413c71935
aptripathi68 <aptripathi@aol.com>
2026-02-23 10:05:29 +0530

    Update webinv.py

--- webinv.py
+++ webinv.py
@@ -1,6 +1,6 @@
-#THE BELOW CODE SUCCESSFUL CAPTURES INVENTORY INPUT, GPS LOCATION, QR CODE, SNAPSHOT 
-AND
-Creation of multiple users possible
+#--------THE BELOW CODE SUCCESSFUL CAPTURES INVENTORY INPUT, GPS LOCATION, QR CODE, SNAPSHOT-------- 
+#--------AND-----------------
+#--------Creation of multiple users possible---------------
 
 
 
